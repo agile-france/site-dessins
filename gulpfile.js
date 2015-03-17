5d5fe4c4d046273421b2115f98d288cd0fd48090
Alvin BERTHELOT <alvin.berthelot@gmail.com>
2015-03-17 17:17:58 +0100

    Add clean public directory when call build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp');
+var del = require('del');
 
-gulp.task('build', function() {
-    gulp.src('./app/assets/**')
-    .pipe(gulp.dest('./public'));
+gulp.task('clean', function (cb) {
+  del([
+    // delete everything under public directory
+    'public/**',
+    // except Git files
+    '!public/.git/**',
+    '!public/.gitignore'
+  ], cb);
+});
+
+gulp.task('build', ['clean'], function() {
+  return gulp.src('app/assets/**')
+    .pipe(gulp.dest('public'));
 });
